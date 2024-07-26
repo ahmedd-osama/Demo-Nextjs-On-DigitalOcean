358d0a562aa6368933a1b4dae980615832d4ee85
Ahmed Osama <112904875+ahmedd-osama@users.noreply.github.com>
2024-07-26 02:36:18 +0200

    deployment with access off

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default async function Home() {
   return (
     <div className="max-w-md w-full mx-auto px-4 py-8 text-center space-y-8">
       <h1 className="text-4xl font-bold text-center">Things updated by github actions again</h1>
+      <p className="text-lg font-bold text-center">access off</p>
 
       {things.length !== 0 && (
         <ul className="divide-y divide-gray-200">
